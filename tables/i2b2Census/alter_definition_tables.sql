b71ca7a35df9fa3050e9d7957b6c678a40ec3b9d
mcneeleyta <mcneeleyta@health.missouri.edu>
2016-06-13 10:49:28 -0500

    Add files via upload

---
+++ tables/i2b2Census/alter_definition_tables.sql
@@ -0,0 +1,10 @@
+alter table [I2B2Census].[acs_20135a_definition] add table_location varchar(100) null
+alter table [I2B2Census].[acs_20135b_definition] add table_location varchar(100) null
+alter table [I2B2Census].acs_cph_2010_sf1a_definition add table_location varchar(100) null
+alter table [I2B2Census].acs_cph_2010_sf1b_definition  add table_location varchar(100) null
+
+
+alter table [I2B2Census].[acs_20135a_definition] add total_column_name varchar(100) null
+alter table [I2B2Census].[acs_20135b_definition] add total_column_name varchar(100) null
+alter table [I2B2Census].acs_cph_2010_sf1a_definition add total_column_name varchar(100) null
+alter table [I2B2Census].acs_cph_2010_sf1b_definition  add total_column_name varchar(100) null
